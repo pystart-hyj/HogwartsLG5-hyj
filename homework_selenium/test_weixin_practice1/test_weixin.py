30518456ff2bb11420dafab278a53a906781bf3f
pystart-hyj <pystart-hyj@email.comgit config --global user.email pystart-hyj@email.com>
2021-01-07 14:04:54 +0800

    更新作业

--- homework_selenium/test_weixin_practice1/test_weixin.py
+++ homework_selenium/test_weixin_practice1/test_weixin.py
@@ -2,11 +2,11 @@ import json
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 
 class TestWinXin():
     def setup(self):
+        # 复用chrome调试窗口
         chrome_options = webdriver.ChromeOptions()
         chrome_options.debugger_address = "127.0.0.1:9222"
         self.driver = webdriver.Chrome(options=chrome_options)
@@ -19,16 +19,20 @@ class TestWinXin():
         self.driver.quit()
 
     def test_cookie(self):
+        # 获取  cookie
         # cookies = self.driver.get_cookies()
         # print(cookies)
+        # 以文件流的形式打开文件
         # with open("cookie.json","w") as f:
+             # 存储 cookie 到 cookie.json
         #     json.dump(cookies, f)
 
         self.driver.get("https://work.weixin.qq.com/")
-
+        # 以文件流的形式打开文件
         with open("cookie.json","r") as f:
+            # 读取 cookies
             cookies = json.load(f)
-
+        # 注入 cookies
         for cookie in cookies:
             self.driver.add_cookie(cookie)
 
